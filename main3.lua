6dd48af090f421d1cc57249937accace1d28b0a6
H1lkaaaGD <h1lkaaagd@hotmail.com>
2025-08-25 13:55:07 +0700

    Update main3.lua

--- main3.lua
+++ main3.lua
@@ -1,5 +1,14 @@
-local Playersc= game:Get service("Players");
-local LocalPlayer = Players.LocalPlayer;
-local Character = LocalPlayer.Character;
+local Players = game:GetService("Players")
 
-Character:Set attribute("CanJump", true)
+local function setupPlayerModel(player)
+    if not player:GetAttribute("CanJump") then
+        player:SetAttribute("CanJump", true)
+        print("Атрибут CanJump создан для игрока", player.Name)
+    end
+end
+
+Players.PlayerAdded:Connect(setupPlayerModel)
+
+for _, player in ipairs(Players:GetPlayers()) do
+    setupPlayerModel(player)
+end
